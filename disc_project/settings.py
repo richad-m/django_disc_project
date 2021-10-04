0bb3f3570664e61ea6d4deece70e5fdf86ab8944
Richad MERALLI <richad.meralli@gmail.com>
2021-10-04 14:41:29 +0200

    updated allowed host for heroku

--- disc_project/settings.py
+++ disc_project/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['disc.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['discstore-rme.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
